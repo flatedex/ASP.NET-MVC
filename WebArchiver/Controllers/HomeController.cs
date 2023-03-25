b206747cb576b18ced54eba42637f5d1d9856db0
flatedex <flatedex@mail.ru>
2023-03-25 20:40:44 +0300

    Uploaded files now adding to database

--- WebArchiver/Controllers/HomeController.cs
+++ WebArchiver/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Razor.Language.Extensions;
+using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
 using System.Diagnostics;
+using WebArchiver.Data;
 using WebArchiver.Models;
 
 namespace WebArchiver.Controllers
@@ -9,14 +12,17 @@ namespace WebArchiver.Controllers
 	{
 		private readonly ILogger<HomeController> _logger;
 		private IWebHostEnvironment _webHostEnvironment;
+		private ApplicationDBContext _db;
+		private readonly int toKbytes = 1024; 
 
 		[BindProperty, Display(Name = "Drag files here")]
 		public List<IFormFile> Uploads { get; set; } = new();
 
-		public HomeController(ILogger<HomeController> logger, IWebHostEnvironment webHostEnvironment)
+		public HomeController(ILogger<HomeController> logger, IWebHostEnvironment webHostEnvironment, ApplicationDBContext db)
 		{
 			_logger = logger;
 			_webHostEnvironment = webHostEnvironment;
+			_db = db;
 		}
 
 		public IActionResult Index()
@@ -37,9 +43,28 @@ namespace WebArchiver.Controllers
 
 			using (FileStream fs = System.IO.File.Create(filePath)) { await file.CopyToAsync(fs); }
 
+			await AddFilesToDB(file);
+
 			Archive();
 
 			return RedirectToAction("Index");
+		}
+		public async Task AddFilesToDB(IFormFile file)
+		{
+			LastFile lf = new LastFile()
+			{
+				Size = file.Length / toKbytes + 1,
+				Name = file.FileName,
+				CreatedDateTime = DateTime.Now,
+				User_id = 0
+			};
+			if(User.Identity.IsAuthenticated)
+			{
+				User loggedUser = _db.User.Where(us => us.Name == User.Identity.Name).ToList().First();
+				lf.User_id = loggedUser.Id;
+			}
+			_db.LastFile.Add(lf);
+			await _db.SaveChangesAsync();
 		}
 		public void Archive()
 		{
